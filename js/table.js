731370677cf4b6207b1a68db6effb9a9c8992554
Seu Nome <cassia.revellianny@escolar.ifrn.edu.br>
2025-12-05 09:54:00 -0300

    vetor e botao excluir

--- js/table.js
+++ js/table.js
@@ -3,19 +3,12 @@ btnOpenPopup.onclick = openPopup;
 btnClose.onclick = closePopup;
 
 function gravar(){
-    let linha = document.createElement("tr");
-    let c1 = document.createElement("td");
-    let c2 = document.createElement("td");
-
-    c1.innerText = inBanda.value;
-    c2.innerText = inMusica.value;
+    newLine(inBanda.value, inMusica.value);
+    
     inBanda.value = "";
     inMusica.value = "";
     inBanda.focus();
 
-    linha.appendChild(c1);
-    linha.appendChild(c2);
-    musicas.appendChild(linha);
     closePopup();
 }
 
@@ -25,4 +18,59 @@ function openPopup(){
 
 function closePopup(){
     popup.style.display = "none";
+}
+
+function newLine(texto1, texto2){
+    let linha = document.createElement("tr");
+    let c1 = document.createElement("td");
+    let c2 = document.createElement("td");
+    let c3 = document.createElement("td");
+
+    c1.innerText = texto1;
+    c2.innerText = texto2;
+    c3.appendChild(createDeleteButton());
+
+    linha.appendChild(c1);
+    linha.appendChild(c2);
+    linha.appendChild(c3);
+    musicas.appendChild(linha);
+    closePopup();
+
+}
+
+function openPopup(){
+    popup.style.display = "block";
+}
+
+function closePopup(){
+    popup.style.display = "none";
+}
+
+let songs = [
+    ["Pitty", "Teto de vidro"],
+    ["Pitty", "Admiravel chip novo"],
+    ["Pitty", "Mácara"],
+    ["Pitty", "Equalize"],
+    ["Pitty", "O lobo"],
+    ["Pitty", "Emboscada"],
+    ["Pitty", "Do mesmo lado"],
+    ["Pitty", "Temporal"],
+    ["Pitty", "I whanna be"],
+    ["Pitty", "Semana que vem"]
+];
+
+songs.forEach((l) => {
+    newLine(l[0], l[1]);
+});
+
+function createDeleteButton(){
+    let btn = document.createElement("button");
+    btn.innerText = "X";
+    btn.className = "delete";
+    btn.onclick = () => {deleteLine(btn)}
+    return btn;
+}
+
+function deleteLine(btn){
+    btn.parentNode.parentNode.remove();
 }
